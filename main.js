5d407ad1df0631ff84c0d61a3b5beb7b39457b48
BlackShade11 <kaczniko135135@gmail.com>
2025-02-19 16:05:39 +0100

    update

--- main.js
+++ main.js
@@ -1,33 +1,39 @@
 $ = (x) => { return document.getElementById(x + ""); }
 
 
-let pixels = new Array(width);
-for (var i = 0; i < pixels.length; i++) {
-    pixels[i] = new Array(height);
-}
+let width = 16;
+let height = 16;
+let scale = 32;
+
+let ctx = $("canvas").getContext("2d");
+
+let pixels;
 
 let tick = 1;
 function reload() {
-    progress = 0;
+    ctx.clearRect(0, 0, width, height);
+    let i = 1;
     for (var x = 0; x < pixels.length; x++) {
         for (var y = 0; y < pixels[x].length; y++) {
-            updateBasicCode(x, y);
+            updateBasicCode(x, y, i);
             let res = runCode();
             pixels[x][y] = res;
+            drawPixel(x, y, res.r, res.g, res.b, res.a);
+            i += 1;
         }
     }
-    render();
+    
     tick++;
 }
 
 let basicCode;
-function updateBasicCode(x, y) {
+function updateBasicCode(x, y, i) {
     basicCode = `
 let pixel = {r:"0", g:"0", b:"0", a:"255"};
 const t = ${tick};
 const x = ${x};
 const y = ${y};
-const i = ${0};
+const i = ${i};
 const w = ${width};
 const h = ${height};
 `
@@ -38,39 +44,28 @@ function runCode() {
     return evalValue;
 }
 
-let ctx = $("canvas").getContext("2d");
-ctx.scale(scale, scale);
-
 function drawPixel(x, y, r, g, b, a) {
     ctx.fillStyle = "rgba(" + r + "," + g + "," + b + "," + (a / 255) + ")";
     ctx.fillRect(x, y, 1, 1);
 }
 
-function render() {
-    ctx.clearRect(0, 0, width, height);
-
-    for (var x = 0; x < pixels.length; x++) {
-        for (var y = 0; y < pixels[x].length; y++) {
-            let p = pixels[x][y];
-            drawPixel(x, y, p.r, p.g, p.b, p.a);
-        }
-    }
-}
-
-
-let width = 32;
-let height = 32;
-let scale = 16;
-updateSize(width, height, scale);
-
 function updateSize(w, h, s) {
-    if(w == undefined || w == null || w == ""){
+    if (w == undefined || w == null || w == "") {
         updateSize($('width').value, $('height').value, $('scale').value);
         return;
     }
     $("canvas").width = w * s;
     $("canvas").height = h * s;
-    width = w;
-    height = h;
-    scale = s;
+    ctx.scale(s, s);
+
+    width = Number(w);
+    height = Number(h);
+    scale = Number(s);
+
+    pixels = new Array(width);
+    for (var i = 0; i < pixels.length; i++) {
+        pixels[i] = new Array(height);
+    }
 }
+
+updateSize(width, height, scale);
